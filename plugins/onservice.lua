5aa2f8323741ae010fd67d0791e9b597ce33dbab
Pouya Poorrahman <poyaskysps@yahoo.com>
2016-08-23 23:06:15 +0430

    Set CopyRight and Set new patterns..Jove V3.1

--- plugins/onservice.lua
+++ plugins/onservice.lua
@@ -1,3 +1,15 @@
+--[[
+|------------------------------------------------- |--------- ______-----------------_______---|
+|   ______   __   ______    _____     _____    __  |  _____  |  ____|  __     __    /  _____/  |
+|  |__  __| |  | |__  __|  /     \   |     \  |  | | |__   | | |____  |  |   |  |  /  /____    |
+|    |  |   |  |   |  |   /  /_\  \  |  |\  \ |  | |   /  /  |  ____| |  |   |  |  \____   /   |
+|    |  |   |  |   |  |  /  _____  \ |  | \  \|  | |  /  /_  | |____  |  |___|  |   ___/  /    |
+|    |__|   |__|   |__| /__/     \__\|__|  \_____| | |_____| |______|  \_______/  /______/     |
+|--------------------------------------------------|-------------------------------------------|
+| This Project Powered by : Pouya Poorrahman CopyRight 2016 Jove Version 3.1 Anti Spam Cli Bot |
+|                             The Other Code Writer: Erfan Kiya                                |
+|----------------------------------------------------------------------------------------------|
+]]
 do
 -- Will leave the group if be added
 local function run(msg, matches)
@@ -7,7 +19,7 @@ local receiver = get_receiver(msg)
        chat_del_user("chat#id"..msg.to.id, 'user#id'..bot_id, ok_cb, false)
 	   leave_channel(receiver, ok_cb, false)
     elseif msg.service and msg.action.type == "chat_add_user" and msg.action.user.id == tonumber(bot_id) and not is_admin1(msg) then
-       send_large_msg(receiver, 'This is not one of my groups.', ok_cb, false)
+       send_large_msg(receiver, '💠This is not one of my groups.<jove3.1>💠', ok_cb, false)
        chat_del_user(receiver, 'user#id'..bot_id, ok_cb, false)
 	   leave_channel(receiver, ok_cb, false)
     end
@@ -16,6 +28,7 @@ end
 return {
   patterns = {
     "^[#!/](leave)$",
+        "^(leave)$",
     "^!!tgservice (.+)$",
   },
   run = run
