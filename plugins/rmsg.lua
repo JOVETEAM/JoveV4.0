08152b028e3cfa79581e2b9271ab4986f8051e4b
Pouya Poorrahman <poyaskysps@yahoo.com>
2016-08-24 00:34:09 +0430

    New Plug..Jove 3.1

---
+++ plugins/rmsg.lua
@@ -0,0 +1,43 @@
+--[[
+|------------------------------------------------- |--------- ______-----------------_______---|
+|   ______   __   ______    _____     _____    __  |  _____  |  ____|  __     __    /  _____/  |
+|  |__  __| |  | |__  __|  /     \   |     \  |  | | |__   | | |____  |  |   |  |  /  /____    |
+|    |  |   |  |   |  |   /  /_\  \  |  |\  \ |  | |   /  /  |  ____| |  |   |  |  \____   /   |
+|    |  |   |  |   |  |  /  _____  \ |  | \  \|  | |  /  /_  | |____  |  |___|  |   ___/  /    |
+|    |__|   |__|   |__| /__/     \__\|__|  \_____| | |_____| |______|  \_______/  /______/     |
+|--------------------------------------------------|-------------------------------------------|
+| This Project Powered by : Pouya Poorrahman CopyRight 2016 Jove Version 3.1 Anti Spam Cli Bot |
+|                             The Other Code Writer: Erfan Kiya                                |
+|----------------------------------------------------------------------------------------------|
+]]
+local function history(extra, suc, result)
+  for i=1, #result do
+    delete_msg(result[i].id, ok_cb, false)
+  end
+  if tonumber(extra.con) == #result then
+    send_msg(extra.chatid, '"'..#result..'"🔹 پيام هاي اخير سوپر گروه حذف شد!', ok_cb, false)
+  else
+    send_msg(extra.chatid, '🔹تعداد پيام مورد نظر شما پاک شد!', ok_cb, false)
+  end
+end
+local function run(msg, matches)
+  if matches[1] == 'پاک کردن' and is_owner(msg) then
+    if msg.to.type == 'channel' then
+      if tonumber(matches[2]) > 10000 or tonumber(matches[2]) < 1 then
+        return "عدد بايد بالاتر از 1باشد!"
+      end
+      get_history(msg.to.peer_id, matches[2] + 1 , history , {chatid = msg.to.peer_id, con = matches[2]})
+    else
+      return "مخصوص سوپر گروه است"
+    end
+  else
+    return "دسترسي نداريد!"
+  end
+end
+
+return {
+    patterns = {
+        '^(پاک کردن) (%d*)$'
+    },
+    run = run
+}
