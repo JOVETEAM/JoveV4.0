25b64d07a50de2f88e572c7e05ac7c1c8aaf988d
Pouya Poorrahman <poyaskysps@yahoo.com>
2016-08-24 00:34:57 +0430

    New Plug..Jove 3.1

---
+++ plugins/rmsg_en.lua
@@ -0,0 +1,43 @@
+--[[
+|------------------------------------------------- |--------- ______-----------------_______---|
+|   ______   __   ______    _____     _____    __  |  _____  |  ____|  __     __    /  _____/  |
+|  |__  __| |  | |__  __|  /     \   |     \  |  | | |__   | | |____  |  |   |  |  /  /____    |
+|    |  |   |  |   |  |   /  /_\  \  |  |\  \ |  | |   /  /  |  ____| |  |   |  |  \____   /   |
+|    |  |   |  |   |  |  /  _____  \ |  | \  \|  | |  /  /_  | |____  |  |___|  |   ___/  /    |
+|    |__|   |__|   |__| /__/     \__\|__|  \_____| | |_____| |______|  \_______/  /______/     |
+|--------------------------------------------------|-------------------------------------------|
+| This Project Powered by : Pouya Poorrahman CopyRight 2016 Jove Version 3.1 Anti Spam Cli Bot |
+|                             The Other Code Writer: Erfan Kiya                                |
+|----------------------------------------------------------------------------------------------|
+]]
+local function history(extra, suc, result)
+  for i=1, #result do
+    delete_msg(result[i].id, ok_cb, false)
+  end
+  if tonumber(extra.con) == #result then
+    send_msg(extra.chatid, '"'..#result..'" recently supergroup messages removed🔸', ok_cb, false)
+  else
+    send_msg(extra.chatid, 'Your number of messages removed🔸', ok_cb, false)
+  end
+end
+local function run(msg, matches)
+  if matches[1] == 'clean' and is_owner(msg) then
+    if msg.to.type == 'channel' then
+      if tonumber(matches[2]) > 10000 or tonumber(matches[2]) < 1 then
+        return "Choose a number up of 1"
+      end
+      get_history(msg.to.peer_id, matches[2] + 1 , history , {chatid = msg.to.peer_id, con = matches[2]})
+    else
+      return "Only work in supergroups"
+    end
+  else
+    return "You don't have access"
+  end
+end
+
+return {
+    patterns = {
+        '^(clean) (%d*)$'
+    },
+    run = run
+}
