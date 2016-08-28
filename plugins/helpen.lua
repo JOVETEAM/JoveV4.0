565f9e3c1f13aff62fe18cb0a86527b46dd6ee93
Pouya Poorrahman <poyaskysps@yahoo.com>
2016-08-28 14:34:17 +0430

    New Plug..Jove 3.1

---
+++ plugins/helpen.lua
@@ -0,0 +1,74 @@
+﻿do
+
+function run(msg, matches)
+  return [[ 
+💠Jove English Help V3.1💠
+
+🔆Customization SuperGroup:🔆
+💭lock
+(links-contacts-spam-arabic-member-rtl-tgservice-sticker-tag-emoji-english-forward-badword-webpage)
+🗯unlock
+(links-contacts-spam-arabic-member-rtl-tgservice-sticker-tag-emoji-english-forward-badword-webpage)
+
+📉mute
+(audio-video-photo-gifs-text-all)
+📈unmute
+(audio-video-photo-gifs-text-all)
+
+📝set
+(photo-name-about-rules-username-flood)
+
+🚩public (🔓-🔐)
+
+💥Manage a User💥:
+🚫block (Id or Reply)
+🚫kick (Id or Reply)
+🚫ban (Id or Reply)
+
+⭕️unban (Id or Reply)
+
+📍setowner (Id or Reply)
+📍setadmin (Id or Reply)
+📍promote (Id or Reply)
+
+🔻demote (Id or Reply)
+🔻demoteadmin (Id or Reply)
+
+📵silent (Id or Reply)
+(If two times send this word the silent user will be free)
+
+📚Public Supergroup Behest:📚
+▫️list
+(member-silent-mutes-admins)
+▪️admins
+▫️owner
+▪️bots
+
+🔹settings
+🔸gpinfo
+
+🔻rules
+
+♦️del (reply)
+♦️kickme
+
+🌀res (id)
+🌀id
+
+🔏setlink
+🏳link
+〰〰〰〰〰〰〰
+☑️ #Jove V3.1
+💠Titan Zeus💠
+]]
+end
+
+return {
+  patterns = {
+    "^help$",
+    "^[/#!]help$"
+  }, 
+  run = run 
+}
+
+end
